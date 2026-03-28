5020e386129f4c17fc0e783c01908f6f16eb4a2c
Syeed141 <shahariarsyeed2026@gmail.com>
2026-03-28 23:42:42 +0600

    Important sections and footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
-import NavBar from "./Layout/NavBar"
-import About from "./Sections/About"
-import Contact from "./Sections/Contact"
-import Expereince from "./Sections/Expereince"
-import Hero from "./Sections/Hero"
-import Project from "./Sections/Project"
-import Testimonial from "./Sections/Testimonial"
+import { Footer } from "./Layout/Footer";
+import NavBar from "./Layout/NavBar";
+import About from "./Sections/About";
+import Contact from "./Sections/Contact";
+import Expereince from "./Sections/Expereince";
+import Hero from "./Sections/Hero";
 
+import { Projects } from "./Sections/Projects";
+import Testimonial from "./Sections/Testimonial";
 
 function App() {
- 
   return (
     <div className="min-h-screen overflow-x-hidden">
-     <NavBar></NavBar>
-    
-     <main>
-      <Hero> </Hero>
-      <About> </About>
-      <Project></Project>
-    <Expereince></Expereince>
-    <Testimonial> </Testimonial>
-    <Contact></Contact>
-     </main>
+      <NavBar></NavBar>
+
+      <main>
+        <Hero> </Hero>
+        <About> </About>
+        <Projects></Projects>
+        <Expereince></Expereince>
+        <Testimonial> </Testimonial>
+        <Contact></Contact>
+      </main>
+
+      <Footer> </Footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
